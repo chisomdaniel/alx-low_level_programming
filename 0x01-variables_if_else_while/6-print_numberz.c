16e1479d4aa1d735291b368a027c85af97216333
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-10 16:53:02 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a = 48;
+	int line = 0;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(line);
+
+	return (0);
+}
