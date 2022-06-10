5e85c01caf5fbc8469fd70767105fe33fed6f8ed
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-10 16:16:56 -0700

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,17 @@
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
-	char a = "abcdefghijklmnopqrstuvwxyz";
-	putchar(a);
+	char a = 'a';
+	char line = '\n';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(line);
 
 	return (0);
 }
