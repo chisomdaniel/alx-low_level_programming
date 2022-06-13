6d7d95d00e698deb79a6b7399bbdc7c31554ffb0
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-13 16:07:48 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: first variable
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	char *a;
+	int count = 0;
+
+	a = s;
+	for (i = 0; i < 200; i++)
+	{
+		if (a[i] == 0)
+		{
+			break;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	return (count);
+}
