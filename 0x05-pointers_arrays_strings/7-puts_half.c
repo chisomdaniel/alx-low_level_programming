38beb85e11ed894eeb49ed6b74144bc7c7a17325
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-14 13:39:38 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a string
+ * _strlen - return length of a string
+ *
+ * @str: puts_half argument
+ * @s: strlen argument
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	char *a;
+	int count = 0;
+
+	a = s;
+	for (i = 0; i < 1000; i++)
+	{
+		if (a[i] == 0)
+		{
+			break;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	return (count);
+}
+
+void puts_half(char *str)
+{
+	int length;
+	int i;
+
+	char *string = str;
+	char b;
+
+	length = _strlen(str);
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i <= length; i++)
+		{
+			b = string[i];
+			_putchar(b);
+		}
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i <= length; i++)
+		{
+			b = string[i];
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+
+}
