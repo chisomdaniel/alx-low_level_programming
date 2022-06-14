74579bbef51d64a434299d80567c1f81c3bcf543
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-13 17:11:38 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: pointer argument
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	char *a;
+	char b;
+	char count = 0;
+
+	a = s;
+	for (i = 0; i < 10000; i++)
+	{
+		if (a[i] == 0)
+		{
+			break;
+		}
+		else
+		{
+			count++;
+		}
+	}
+
+	while (count >= 0)
+	{
+		b = a[count - 1];
+		_putchar(b);
+		count--;
+	}
+
+	_putchar('\n');
+}
