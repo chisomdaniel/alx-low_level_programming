ec28a61a5c753cf7c309203f5de581793e021579
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-14 13:43:00 -0700

    error

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -44,7 +44,7 @@ void puts_half(char *str)
 
 	if (length % 2 == 0)
 	{
-		for (i = length / 2; i <= length; i++)
+		for (i = length / 2; i < length; i++)
 		{
 			b = string[i];
 			_putchar(b);
@@ -52,7 +52,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = (length - 1) / 2; i <= length; i++)
+		for (i = (length - 1) / 2; i < length; i++)
 		{
 			b = string[i];
 			_putchar(b);
