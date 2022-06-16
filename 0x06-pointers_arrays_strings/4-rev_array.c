4da88745b09bdaa79da07b197725d6b6074efe7e
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-16 03:09:55 -0700

    reverse a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - function to revers an array of int
+ *
+ * @a: array of int
+ * @n: number of elements in the array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	int hold[9999];
+
+	for (i = 1; i <= n; i++)
+	{
+		hold[i] = a[n - i];
+		printf("%d, ", hold[i]);
+	}
+	printf("\n");
+
+	for (j = 1; j <= n; j++)
+	{
+		a[j] = hold[j];
+		printf("%d, ", a[j]);
+	}
+	printf("\n");
+}
