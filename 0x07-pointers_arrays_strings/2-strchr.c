28957f016be2a67172df8da80f965d296a46aea4
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-20 15:46:55 -0700

    locate a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr - function to locate a character c in s
+ * 
+ * @s: the main string
+ * @c: character to locate
+ * 
+ * Return: a pointer
+ * 
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (0);
+
+}
