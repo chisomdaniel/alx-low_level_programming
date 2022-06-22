f4413af93367e8b525968dab6b067e015a136390
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-22 14:55:47 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,17 +9,16 @@
 
 int _pow_recursion(int x, int y)
 {
-    int a;
+	
+	if (y < 0)
+	{
+		return (-1);
+	}
 
-    if (y < 0)
-    {
-        return (-1);
-    }
+	if (y <= 0)
+	{
+		return (1);
+	}
 
-    if (y <= 0)
-    {
-        return (1);
-    }
-
-    a = x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
