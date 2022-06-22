e29b4c62bda92927212885e7c5aea6459e707b56
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-22 14:45:41 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen_recursion - main function
+ *
+ * @s: first function argument
+ *
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
