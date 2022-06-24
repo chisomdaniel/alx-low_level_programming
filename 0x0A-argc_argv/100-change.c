ec3f201d840751f48e2fd69ef057a93422736834
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-24 15:08:52 -0700

    make change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: argument count
+ * @argv: string argument
+ *
+ * Return: return a number
+ */
+
+int main(int argc, char **argv)
+{
+	int cent[5] = {25, 10, 5, 2, 1};
+	int i;
+	int money, count = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	money = atoi(argv[1]);
+	if (money <= 0)
+	{
+		printf("0");
+		return(0);
+	}
+
+	for (i = 0; i <= 5; i++)
+	{
+		for(; money - cent[i] >= 0 && money > 0;)
+		{
+			money -= cent[i];
+			count++;
+		}
+	}
+	printf("%i\n", count);
+
+	return (0);
+}
+
