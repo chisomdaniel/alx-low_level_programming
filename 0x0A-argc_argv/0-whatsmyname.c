729f3a06b36fcbc7e0d4343e7b823728dcb60bff
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-24 07:06:36 -0700

    print name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: length of argv
+ * @argv: the passed variable
+ *
+ * Return: 0 Always
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
