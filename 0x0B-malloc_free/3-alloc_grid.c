58392759c75a44c205c3fff1f874ff4b291f5cec
Chisom Daniel <chinwezechisom@gmail.com>
2022-06-28 06:58:43 -0700

    error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		/* Alocate for the column, or pointer under the main one */
-		arr[i] = malloc(sizeof(arr) * width);
+		arr[i] = malloc(sizeof(arr) * width + 1);
 		if (arr[i] == NULL)
 		{
 			return (NULL);
