a7533a5617e93e3490579ff242f870a9025f0f4a
Chisom Daniel <chinwezechisom@gmail.com>
2022-07-06 06:06:46 -0700

    search for the unsearchable

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+/**
+ * int_index - search for a string
+ *
+ * @array: the int array
+ * @size: the size of the array
+ * @cmp: a function pointer
+ *
+ * Return: the index of the first element for which 
+ * the cmp function does not return 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	while (size--)
+	{
+		if (cmp[i] != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+
+	return(-1);
+}
