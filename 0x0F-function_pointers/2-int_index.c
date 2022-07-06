959d63a01cf105f99362a2bf3ab31fff31953099
Chisom Daniel <chinwezechisom@gmail.com>
2022-07-06 16:06:47 -0700

    search int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * int_index - search for a string
  *
@@ -5,13 +7,18 @@
  * @size: the size of the array
  * @cmp: a function pointer
  *
- * Return: the index of the first element for which 
+ * Return: the index of the first element for which
  * the cmp function does not return 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1)
+	}
 
 	if (size <= 0)
 	{
@@ -27,5 +34,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		i++;
 	}
 
-	return(-1);
+	return (-1);
 }
