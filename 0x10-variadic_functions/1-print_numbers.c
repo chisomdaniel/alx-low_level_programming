8b6fcd2a38a6611e2bc0e6bb787a37dae9b664fe
Chisom Daniel <chinwezechisom@gmail.com>
2022-07-07 12:07:53 -0700

    print num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		a = va_arg(ap, int);
-		if (separator != NULL || separator != "")
+		if (separator != NULL || "")
 		{
 			printf("%d%s", a, separator);
 		}
