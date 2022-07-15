e00019099c0f167fa0d60b03880a0b991dce499c
Chisom Daniel <chinwezechisom@gmail.com>
2022-07-15 09:04:41 -0700

    add to the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,6 +28,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	temp = malloc(sizeof(list_t));
+	if (temp = NULL)
+		return (NULL);
+
 	temp->str = strdup(str);
 	temp->len = len;
 	temp->next = NULL;
