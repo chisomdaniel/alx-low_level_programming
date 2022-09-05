ee6db170fb9d37bfa383859c5e01a41a8dbebc59
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-05 15:47:10 +0100

    shared library with python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,69 @@
+/**
+ * add - a function to add numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - a function to substract numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - a function to multiply numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - a function to divide numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - a function to give the mod of a numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
