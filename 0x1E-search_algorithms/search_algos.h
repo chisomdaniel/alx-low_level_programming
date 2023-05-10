c834815a3f20a234259c999d2fd73594d9f1101c
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-10 21:35:29 +0100

    advance jump 2

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,9 +6,30 @@
 #include <math.h>
 
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *arr, int a, int b);
 int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+void print_array_2(int *arr, int end);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *search(listint_t *head, size_t index);
 
 #endif
